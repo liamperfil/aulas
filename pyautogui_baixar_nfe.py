b03c69aed81579045873e01f7c2dbe91384a8571
Liam <143908710+liamperfil@users.noreply.github.com>
2024-04-02 11:39:08 -0300

    Update pyautogui_baixar_nfe.py

--- pyautogui_baixar_nfe.py
+++ pyautogui_baixar_nfe.py
@@ -1,4 +1,4 @@
-# python baixar.py
+# python pyautogui_baixar_nfe.py
 # navegador como primeiro programa da barra
 #   primeira aba do navegador planilha nf com foco na chave de acesso https://docs.google.com/spreadsheets/d/1b2Nr-pdD6nDzkVTzLoq0l9YzGiykN4SCcIdT7X3Eerc/edit#gid=285781729
 #   segunda aba do navegador portal nfe com o captcha já marcado
@@ -7,10 +7,10 @@
 
 import pyautogui
 import time
-pyautogui.PAUSE = 0.6
+pyautogui.PAUSE = 0.5
 
 def rodar():
-    i = 14 #número de repetição
+    i = 1 #número de repetição
     pyautogui.hotkey('winleft', '1') #navegador
     while (i > 0):
         i -= 1;
@@ -30,7 +30,6 @@ def rodar():
         time.sleep(0.4)
         pyautogui.click(x=872, y=441) #nova consulta 
         time.sleep(0.2)
-        time.sleep(5) #deletar
         pyautogui.click(x=1036, y=538) #captcha
         pyautogui.click(x=1036, y=538) #captcha
 
