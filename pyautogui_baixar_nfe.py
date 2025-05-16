1c0531050b026b660cffc93d1983df4afe7fbf27
Jean Lima <jeancarlos.ramos@live.com>
2025-05-16 10:45:29 -0300

    Update pyautogui_baixar_nfe.py

--- pyautogui_baixar_nfe.py
+++ pyautogui_baixar_nfe.py
@@ -1,4 +1,4 @@
-# 1- navegador modo convidado em tela cheia como primeiro programa da barra: 1 estoque 2 nfe
+# 1- navegador modo ANONIMO, tela cheia, como primeiro programa da barra: aba 1 estoque, aba 2 nfe
 # 2- prompt como segundo programa da barra
 # 3- fazer a primeira vez manualmente para reconhecer o certificado antes de executar o programa
 
@@ -14,47 +14,45 @@ pyautogui.PAUSE = 0.4
 
 def rodar():
     pyautogui.hotkey('winleft', '1') #navegador
-    i = 7
+    i = 40
     while (i > 0):
+        print(i)
         i -= 1
         pyautogui.hotkey('ctrlleft', '1') # aba 1 planilha
         pyautogui.press('down')
         pyautogui.hotkey('ctrlleft', 'c')
               
         pyautogui.hotkey('ctrlleft', '2') # aba 2 portal nfe
-        pyautogui.press('tab', presses=37, interval=0.1)
+        
+        pyautogui.click(x=1078, y=470)
         pyautogui.hotkey('ctrlleft', 'v') # formulario chave nfe
         
-        pyautogui.press('tab', presses=2, interval=0.3)
-        pyautogui.click(x=1035, y=506) # captcha
-        pyautogui.click(x=1035, y=506) # captcha
-        # pyautogui.press('enter', presses=3, interval=0.3) # captcha
-        
-        #pyautogui.press('tab')
-        pyautogui.press('tab', presses=2, interval=0.3)
-        time.sleep(0.6)
+        pyautogui.press('tab', presses=5, interval=0.04)
         pyautogui.press('enter') # continuar
-        time.sleep(0.6)
+        time.sleep(2)
 
-        pyautogui.press('tab', presses=39, interval=0.1)
-        time.sleep(0.6)
+        pyautogui.press('tab', presses=39, interval=0.08)
+        
+        # bloco de depuração
+        # pyautogui.hotkey('winleft', '2') #prompt
+        # input("Tudo certo até aqui? Pressione ENTER")
+        # pyautogui.hotkey('winleft', '1') #navegador
+        
+        time.sleep(1)
         pyautogui.press('enter') # download
         pyautogui.press('espace') # download
-        time.sleep(3)
+        time.sleep(1.5)
         
         pyautogui.press('enter') # dialogo alerta
-        time.sleep(0.8)
-        
-        pyautogui.press('tab', presses=3, interval=0.2)
-        pyautogui.press('enter') # dialogo salvar
-        time.sleep(0.6)
+        time.sleep(1.5)
         
         pyautogui.hotkey('shift', 'tab')
-        time.sleep(0.6)
         pyautogui.hotkey('shift', 'tab')
-        time.sleep(1)
         pyautogui.press('enter', presses=3, interval=0.3) # nova consulta
-        time.sleep(2)
+        time.sleep(1)
+
+        pyautogui.click(x=1033, y=530)
+        pyautogui.click(x=1033, y=530)
 
 rodar()
 pyautogui.hotkey('winleft', '2') #prompt
