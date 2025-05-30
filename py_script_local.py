2ec2a707869c0b6e828acd80901642194f386053
liamperfil <jeancarlos.ramos@live.com>
2025-05-30 14:45:07 -0300

    update

--- py_script_local.py
+++ py_script_local.py
@@ -1,4 +1,9 @@
-with open(r'C:\ambiente_virtual\senha.py', 'rb') as resposta:
+# Este código abre o arquivo 'dados.py' em modo binário, lê todo o seu conteúdo,
+# decodifica de bytes para string e executa o código Python contido nele usando a função exec().
+with open(r'C:\Users\jeanc\OneDrive\Github\aulas\arquivo.py', 'rb') as resposta:
     conteudo_arquivo = resposta.read()
-
 exec(conteudo_arquivo.decode())
+
+# Uma forma mais eficiente é importar o módulo diretamente, se ele estiver no mesmo diretório:
+import arquivo
+# Agora você pode usar as funções e variáveis definidas no módulo 'arquivo.py'
